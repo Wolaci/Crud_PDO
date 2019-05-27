c1af80ad8e35f2bf83d3e7e26253406f993425a1
Wolaci <wolacijose8@gmail.com>
2019-05-27 16:52:48 -0300

    Implementação da máscara

--- app/index.php
+++ app/index.php
@@ -5,11 +5,13 @@
 	<title>Cadastro</title>
 </head>
 <body>	
-	<form action="">
+	<form action="" name="form1">
 		Nome: <input type="text" name="nome">
-		Telefone: <input type="text" name="telefone">
-		Cidade: <input type="text" name="cidade">
-		Estado: <select name="estado">
+		<br><br>Telefone: 
+		<input type="text" name="tel" onKeyPress="MascaraTelefone(form1.tel);" 
+		maxlength="14"  onBlur="ValidaTelefone(form1.tel);">
+		<br><br>Cidade: <input type="text" name="cidade">
+		<br><br>Estado: <select name="estado">
 			<option value="">Selecione eu Etado</option>
 			<option value="Acre">Acre (AC)</option>
 			<option value="Alagoas">Alagoas (AL)</option>
@@ -39,12 +41,18 @@
 			<option value="Sergipe">Sergipe (SE)</option>
 			<option value="Tocantins">Tocantins (TO)</option>
 		</select>
-		E-mail: <input type="text" name="email">
-		Informações adicionais: <input type="text" name="informacao">
-Tipo cliente:
-<input type="radio" name="opcao" value="op1"> Física
-<input type="radio" name="opcao" value="op2"> Jurídica 
- 
+		<br><br>E-mail: <input type="text" name="email">
+		<br><br>Informações adicionais: <input type="text" name="informacao">
+		<br><br>Tipo cliente:
+		<input type="radio" name="opcao" value="op1"> Física
+        <input type="radio" name="opcao" value="op2"> Jurídica 
+		<br><br>CPF:
+		<input type="text" name="cpf" onBlur="ValidarCPF(form1.cpf);" 
+		onKeyPress="MascaraCPF(form1.cpf);" maxlength="14">
+		<br><br>CNPJ:
+		<input type="text" name="cnpj" onKeyPress="MascaraCNPJ(form1.cnpj);" 
+		maxlength="18" onBlur="ValidarCNPJ(form1.cnpj);">
 	</form>
+
 </body>
 </html>
