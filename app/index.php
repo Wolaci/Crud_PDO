9bb9348b23b1570b0cc93fc324c78c4c249c1c1e
Wolaci <wolacijose8@gmail.com>
2019-05-27 15:02:16 -0300

    Criação do index.php com o formulário

---
+++ app/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Cadastro</title>
+</head>
+<body>	
+	<form action="">
+		Nome: <input type="text" name="nome">
+		Telefone: <input type="text" name="telefone">
+		Cidade: <input type="text" name="cidade">
+		<select name="estado" id="">
+			<options value="">Selecione seu Estado</options>
+			<options value=""></options>
+			<options value=""></options>
+			<options value=""></options>
+			<options value=""></options>
+			<options value=""></options>
+			<options value=""></options>
+			<options value=""></options>
+			<options value=""></options>
+			<options value=""></options>
+			<options value=""></options>
+			<options value=""></options>
+			<options value=""></options>
+			<options value=""></options>
+			<options value=""></options>
+			<options value=""></options>
+			<options value=""></options>
+			<options value=""></options>
+			<options value=""></options>
+			<options value=""></options>
+			<options value=""></options>
+			<options value=""></options>
+			<options value=""></options>
+			<options value=""></options>
+		</select>
+		E-mail: <input type="text" name="email">
+		Informações adicionais: <input type="text" name="informacao">
+		Tipo Cliente: <input type="radio" name="fisica"> <input type="radio" name="Juridica">
+		CPF / CNPJ<input type="text" name="cpf">
+	</form>
+</body>
+</html>
